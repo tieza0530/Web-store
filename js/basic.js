0d660599cacdb3156cca293a5646a52512848231
tieza0530 <tiennq201@gmail.com>
2024-07-06 21:47:30 +0700

    web-ban-hang

---
+++ js/basic.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('#menu')
+
+function menuList() {
+    menu.classList.add('displayBlock')
+}
+
+function del_menu() {
+    menu.classList.remove('displayBlock')
+
+}
